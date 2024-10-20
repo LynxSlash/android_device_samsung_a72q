8d343153ed426a62e45e9f1a26c21cba2870066b
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-10-20 07:22:09 +0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,9 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/samsung/a72q
+BOARD_VENDOR := samsung
+
+DEVICE_PATH := device/samsung/a70q
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := false
@@ -27,18 +29,20 @@ BOARD_AVB_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_SYSTEM_ALGORITHM := SHA256_RSA2048
 
 # Architecture
+# Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
-# 2nd Architecture
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 
 TARGET_SUPPORTS_64_BIT_APPS := true
 
@@ -50,45 +54,51 @@ ENABLE_SCHEDBOOST := true
 TARGET_USES_UEFI := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := a72q
+TARGET_OTA_ASSERT_DEVICE := a70q
 
 # Kernel: Base flags
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image
-BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.avb_version=1.0 androidboot.vbmeta.avb_version=1.0
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
+BOARD_HEADER_VERSION     := 1
 # Kernel: Board (kernel...) flags
-BOARD_NAME               := SRPTJ06B001
+BOARD_NAME               := SRPRL06C005
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
-BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_KERNEL_OFFSET      := 0x00008000
 BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_SECOND_OFFSET      := 0x00f00000
 
 # Kernel: mkbootimgs args
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS += --board $(BOARD_NAME)
+BOARD_MKBOOTIMG_ARGS     := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --second_offset $(BOARD_SECOND_OFFSET) --header_version $(BOARD_HEADER_VERSION) --board $(BOARD_NAME)
 
 # Prebuilt: Kernel
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
-BOARD_KERNEL_IMAGE_NAME := Image.gz
-
-# Prebuilt: DTB
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Prebuilt: DTBO
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 BOARD_INCLUDE_RECOVERY_DTBO := true
 
+# Kernel - prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_KERNEL_SEPARATED_DTBO := 
+endif
+
 # Platform: Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := atoll
+TARGET_USES_UEFI := true
 
 # Platform: Board
-TARGET_BOARD_PLATFORM := atoll
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno618
+TARGET_BOARD_PLATFORM := sm6150
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno612
 QCOM_BOARD_PLATFORMS += $(TARGET_BOARD_PLATFORM)
 
 # Encryption: Setup it
@@ -98,12 +108,14 @@ BOARD_USES_QCOM_FBE_DECRYPTION := true
 BOARD_USES_METADATA_PARTITION := true
 
 # File systems
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE   := ext4
-BOARD_SYSTEMIMAGE_PARTITION_TYPE     := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE   := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
+BOARD_USES_METADATA_PARTITION        := true
 
 # Target copy out
 TARGET_COPY_OUT_VENDOR := vendor
@@ -113,49 +125,97 @@ BOARD_ROOT_EXTRA_FOLDERS := persist efs sec_efs firmware
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Partition: Size
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 81788928
-BOARD_BOOTIMAGE_PARTITION_SIZE     := 100663296
-BOARD_DTBOIMAGE_PARTITION_SIZE     := 25165824
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_DTBOIMG_PARTITION_SIZE := 10485760
+BOARD_CACHEIMAGE_PARTITION_SIZE := 335544320
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 82825216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5510266880
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 119370772480
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1048576000
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 BOARD_HAS_NO_SELECT_BUTTON := true
 RECOVERY_SDCARD_ON_DATA := true
-BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 LZMA_RAMDISK_TARGETS := recovery
 
-# Partitions
-BOARD_SUPER_PARTITION_GROUPS := main
-BOARD_SUPER_PARTITION_SIZE := 7730906700
-BOARD_MAIN_SIZE := 7730906700
-BOARD_MAIN_PARTITION_LIST := system odm vendor product
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# TWRP Configuration: Basic config
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+
+# Root
+BOARD_ROOT_EXTRA_FOLDERS := efs metadata
+
+# Security
+VENDOR_SECURITY_PATCH := 2023-02-01
+
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
+
+# TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_INCLUDE_NTFS_3G := true
-TW_NO_EXFAT_FUSE := true
-TW_Y_OFFSET := 142
-TW_H_OFFSET := -142
+TW_USE_TOOLBOX := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone18/temp"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 180
+TW_MAX_BRIGHTNESS := 255
+TW_Y_OFFSET := 77
+TW_H_OFFSET := -77
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_LEGACY_PROPS := true
+TW_NO_REBOOT_BOOTLOADER := true
+
+## TARGET_RECOVERY_QCOM_RTC_FIX := true
+## TW_INCLUDE_CRYPTO := true
+## TW_INCLUDE_LPDUMP := true
+## TW_INCLUDE_LPTOOLS := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_INCLUDE_FUSE_NTFS := true
 TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
 TW_INCLUDE_RESETPROP := true
 
-# TWRP Configuration: Brightness/CPU
-TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone17/temp
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
-TW_MAX_BRIGHTNESS := 486
-TW_DEFAULT_BRIGHTNESS := 219
-
-# TWRP Configuration: Logd
+# Debugging
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
+# SHRP specific build flags
+SHRP_PATH := device/samsung/a70q
+SHRP_MAINTAINER := lynxSlash
+SHRP_DEVICE_CODE= a70q
+SHRP_REC_TYPE := SAR
+SHRP_DEVICE_TYPE := A_Only
+SHRP_STATUSBAR_RIGHT_PADDING := 48
+SHRP_STATUSBAR_LEFT_PADDING := 48
+SHRP_NOTCH := true
+SHRP_EXPRESS := true
+SHRP_DARK := true
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 1
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+SHRP_NO_SAR_AUTOMOUNT := true
+#SHRP_CUSTOM_FLASHLIGHT := true
+#SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
+#SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
+#SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
+
 # Backups
 TW_BACKUP_EXCLUSIONS := /data/fonts
+
+
+# enable python3
+TW_INCLUDE_PYTHON := true
